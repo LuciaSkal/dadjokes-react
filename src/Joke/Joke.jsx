eea0e0dffe7ff3abf2831961c7d2aec06531d9e9
Lucia Skalinova <skalinovalucia@gmail.com>
2021-05-29 15:29:52 +0200

    dokonceni tlacitek, a h1 nadpisu

--- src/Joke/Joke.jsx
+++ src/Joke/Joke.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import './style.css';
+import { useState } from 'react';
 
-const Joke = () => {
+
+
+const Joke = ({avatar, name, text, likes, dislikes}) => {
+  
+  const [pocetKlikuLike, setPocetKlikuLike] = useState(likes);
+  const [pocetKlikuDislike, setPocetKlikuDislike] = useState(dislikes);
+
+  const handleSelectLike = () => {
+    setPocetKlikuLike(pocetKlikuLike + 1);
+  };
+  
+  const handleSelectDislike = () => {
+    setPocetKlikuDislike(pocetKlikuDislike + 1);
+  };
 
   return (
     <div className="container">
       <div className="joke">
         <div className="joke__body">
           <div className="joke__user">
-            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
-            <p className="user-name">Neroxx</p>
+            <img className="user-avatar" src={avatar} />
+            <p className="user-name">{name}</p>
           </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
+            <p className="joke__text">{text} </p>
         </div>
         <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up"></button>
-          <span id="likes-up" className="likes-count likes-count--up">0</span>
-          <button id="btn-down" className="btn-like btn-like--down"></button>
-          <span id="likes-down" className="likes-count likes-count--down">0</span>
+          <button id="btn-up" className="btn-like btn-like--up" onClick={handleSelectLike}></button>
+          <span id="likes-up" className="likes-count likes-count--up">{pocetKlikuLike}</span>
+          <button id="btn-down" className="btn-like btn-like--down" onClick={handleSelectDislike}></button>
+          <span id="likes-down" className="likes-count likes-count--down">{pocetKlikuDislike}</span>
         </div>
       </div>
     </div>
   )
 };
 
-export default Joke
+export default Joke;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Joke from './Joke/Joke';
+import { jokes } from './jokes';
 import './style.css';
 
-const App = () => (
 
-  <div className="container">
-    
-     <Joke />
-    
-  </div>
- );
+const App = () => {
 
+  return (
+     <>
+     <h1>Jokes 😂</h1>
+     {jokes.map((joke) => 
+     <Joke avatar={joke.avatar}
+           name={joke.name}  
+           text={joke.text} 
+           likes={joke.likes}
+           dislikes={joke.dislikes}
+           key={joke.id}
+     />
+     )};
+    </>
+  )
+};
 
 render(<App />, document.querySelector('#app'));
